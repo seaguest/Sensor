b3a46341f0d14dbaf4c22e5f89deae3e8cb07f07
Yongkan HUANG <huangyo@ensimag.fr>
2011-06-14 11:45:48 -0400

    final

--- Create_Network/Network/synchrone.h
+++
@@ -1,7 +0,0 @@
-#ifndef SYNCHRONE_H 
-#define SYNCHRONE_H 
- 
-void Delay_Rand();			//wait for 0 ~ 100 ms
-void Synchrone_Init(uint8_t mac);
-
-#endif
--- Create_Network/Network/uart.h
+++
@@ -1,7 +0,0 @@
-#ifndef UART_H 
-#define UART_H 
-
-void Uart_Init();				//initialisation de uart
-void TXString(char* string, int length);	//send message in uart
-
-#endif
